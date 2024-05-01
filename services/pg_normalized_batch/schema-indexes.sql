c2a0dd3fe01214a506817c990f4f5c26c1849fbe
GusAlbach <zepolalbach@gmail.com>
2024-05-01 16:06:32 -0700

    indexes

--- services/pg_normalized_batch/schema-indexes.sql
+++ services/pg_normalized_batch/schema-indexes.sql
@@ -1,8 +1,8 @@
 SET max_parallel_maintenance_workers TO 50;
 SET max_parallel_workers TO 50;
 SET maintenance_work_mem TO '4 GB';
-CREATE INDEX ON tweet_tags(id_tweets, tag);
-CREATE INDEX ON tweet_tags(tag, id_tweets);
-CREATE INDEX ON tweets(id_tweets, lang);
-CREATE INDEX ON tweets(lang);
-CREATE INDEX ON tweets USING GIN(to_tsvector('english', text)) where lang = 'en';
+CREATE INDEX a1 ON tweet_tags(id_tweets, tag);
+CREATE INDEX a2 ON tweet_tags(tag, id_tweets);
+CREATE INDEX a3 ON tweets(id_tweets, lang);
+CREATE INDEX a4 ON tweets(lang);
+CREATE INDEX a5 ON tweets USING GIN(to_tsvector('english', text)) where lang = 'en';
